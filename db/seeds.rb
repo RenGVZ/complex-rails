bc332bc824f0e6dafa0a5b5789f21d5db9b47aaf
gavingarza <gaving@live.com>
2020-07-08 23:06:21 +0900

    modified the home file to support active record models for barber data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,38 @@ barber = Barber.new(
   barber.save!
 
   puts 'barber 1 created'
+
+  barber = Barber.new(
+    name: 'Ced', 
+    instagram: 'https://www.instagram.com/lamborghinimoss24/',
+    booksy: 'https://booksy.com/en-us/201925_ced-moss_barber-shop_39131_pasco'
+    )
+    file  = File.open(File.join(Rails.root,'app/assets/images/staff2.jpg'))
+    barber.photo.attach(io: file, filename: 'staff2.jpg', content_type: 'image/jpg') 
+    barber.save!
+  
+    puts 'barber 2 created'
+
+  barber = Barber.new(
+    name: 'Bert', 
+    instagram: 'https://www.instagram.com/',
+    booksy: 'https://booksy.com/en-us/201922_humberto-mendez_barber-shop_39131_pasco'
+    )
+    file  = File.open(File.join(Rails.root,'app/assets/images/staff3.jpg'))
+    barber.photo.attach(io: file, filename: 'staff3.jpg', content_type: 'image/jpg') 
+    barber.save!
+
+  puts 'barber 3 created'
+
+  barber = Barber.new(
+    name: 'Erik', 
+    instagram: 'https://www.instagram.com/',
+    booksy: 'https://booksy.com/en-us/201926_erik-mendoza_barber-shop_39131_pasco'
+    )
+    file  = File.open(File.join(Rails.root,'app/assets/images/staff4.jpg'))
+    barber.photo.attach(io: file, filename: 'staff4.jpg', content_type: 'image/jpg') 
+    barber.save!
+
+  puts 'barber 3 created'
+
+  
