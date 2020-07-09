4c50d1bc7d22f809e7e399d8b1126201fc4827c6
gavingarza <gaving@live.com>
2020-07-09 22:50:53 +0900

    changed and tweaked product cards to display description and animate on hover

--- app/javascript/components/cards.js
+++ app/javascript/components/cards.js
@@ -2,18 +2,12 @@ const cardHover = () => {
   const cards = document.querySelectorAll('.card-category');
   cards.forEach(card => {
     card.addEventListener('mouseenter', (e) => {
-      console.log(e.offsetX)
-      // e.offsetX = 100
       card.classList.toggle('card-hover');
     });
-    
     card.addEventListener('mouseleave', () => {
       card.classList.toggle('card-hover');
     })
   })
-  // cards.forEach.addEventListener('click', (e) => {
-  //   console.log(e)
-  // });
 }
 
 export { cardHover };
