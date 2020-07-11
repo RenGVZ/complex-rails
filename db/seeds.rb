e2d04067015fdf3050fce63e52ae597be87eb680
gavingarza <gaving@live.com>
2020-07-11 23:49:32 +0900

    created product seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 
+# Create barbers
 barber = Barber.new(
   name: 'Abel', 
   instagram: 'https://www.instagram.com/abel.garza/',
@@ -51,4 +52,36 @@ barber = Barber.new(
 
   puts 'barber 4 created'
 
-  
+  # Create products
+  product = Product.new(
+    name: 'Comb',
+    description: 'A fine-toothed comb. Excelent for all types of hair. Trusted by barbers worldwide',
+    price: 12,
+    url: 'amazon.com'
+    )
+    file  = File.open(File.join(Rails.root,'app/assets/images/product1.jpg'))
+    product.photo.attach(io: file, filename: 'product1.jpg', content_type: 'image/jpg') 
+    product.save!
+  puts 'product 1 created'
+
+  product = Product.new(
+    name: 'Tonic',
+    description: 'A tonic for better hair treatment. Use this 3 times per day to see noticable differences to your hair\'s overall look and feel.',
+    price: 24,
+    url: 'amazon.com'
+    )
+    file  = File.open(File.join(Rails.root,'app/assets/images/product2.jpg'))
+    product.photo.attach(io: file, filename: 'product2.jpg', content_type: 'image/jpg') 
+    product.save!
+  puts 'product 2 created'
+
+  product = Product.new(
+    name: 'Pommade',
+    description: 'Styling pommade. Get the exact look you want with this pommade. It gives you a hard hold and it\'s easy to wash off.',
+    price: 18,
+    url: 'amazon.com'
+    )
+    file  = File.open(File.join(Rails.root,'app/assets/images/product3.jpg'))
+    product.photo.attach(io: file, filename: 'product3.jpg', content_type: 'image/jpg') 
+    product.save!
+  puts 'product 3 created'
